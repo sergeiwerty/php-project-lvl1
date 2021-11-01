39479a14751d7fdd1f2f523a44436494cc40f0f6
sergeiwerty <dshl.srp@gmail.com>
2021-11-01 10:43:44 +0300

    add brain-even game

--- src/brain-games/Cli.php
+++ src/brain-games/Cli.php
@@ -2,16 +2,6 @@
 
 namespace Brain\Games\Cli;
 
-//use Cli\Out;
-//  require_once __DIR__ . '/../vendor/autoload.php';
-
-
-//  use function cli\line;
-//  use function cli\prompt;
-
-//  line('Welcome to the Brain Game!');
-//  $name = prompt('May I have your name?');
-//  line("Hello, %!", $name);
 function greet()
 {
     \cli\line('Welcome to the Brain Game!');
